01a3883d05df2c9c7d3b92f641d268f9cf532f82
burnworks <6436833+burnworks@users.noreply.github.com>
2023-12-15 15:57:04 +0900

    add z-index

--- show-heading-level/show-heading-level.js
+++ show-heading-level/show-heading-level.js
@@ -4,6 +4,7 @@ javascript: (() => {
         const label = document.createElement('div');
         label.innerText = heading.tagName;
         label.style.position = 'absolute';
+        label.style.zIndex = '9999';
         label.style.backgroundColor = '#B71C1C';
         label.style.color = 'white';
         label.style.padding = '0.125rem 0.25rem';
